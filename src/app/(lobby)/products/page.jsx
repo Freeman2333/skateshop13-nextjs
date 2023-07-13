4ed572c08c851ef7aa58fdbe2da3dc6b8389798e
Serhii Popov <serhii.popov@codeit.pro>
2023-07-13 13:00:31 +0300

    sidebar added

--- src/app/(lobby)/products/page.jsx
+++ src/app/(lobby)/products/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Box } from "@mui/material";
 
+import { siteConfig } from "@/config/site";
 import PageHeader from "@/components/page-header";
 import Products from "@/components/products";
 import Pagination from "@/components/pagination";
+import ProductFilterSidebar from "@/components/product-filter-sidebar";
 
 const ProductsPage = ({ searchParams }) => {
   const { page } = searchParams;
@@ -14,6 +16,7 @@ const ProductsPage = ({ searchParams }) => {
         title={"products"}
         description="Buy products from our stores"
       />
+      <ProductFilterSidebar categories={siteConfig.mainNav} />
       <Products />
       <Pagination count={10} />
     </Box>
