b3f28238b4c40aa1527c5a9fb836b23d76111334
Serhii Popov <serhii.popov@codeit.pro>
2023-07-18 14:00:15 +0300

    markup added

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "dummyimage.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
---
+++ src/app/(lobby)/product/[productId]/page.jsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import {
+  Box,
+  Typography,
+  Divider,
+  Grid,
+  Button,
+  TextField,
+  Chip,
+} from "@mui/material";
+
+import { query } from "@/db";
+
+const ProductPage = async ({ params }) => {
+  const { productId } = params;
+
+  const productQuery = `
+        SELECT p.*, c.name AS categoryName
+        FROM product p
+        JOIN category c ON p.categoryid = c.id
+        WHERE p.id = ?;
+  `;
+
+  const [product] = await query({ query: productQuery, values: [productId] });
+
+  console.log({ product });
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} lg={6}>
+        <Image
+          src="http://dummyimage.com/600x400.png/dddddd/000000"
+          width={600}
+          height={400}
+          alt="Product Image"
+        />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Box p={2}>
+          <Typography variant="h4">{product.name}</Typography>
+          <Typography variant="subtitle1" color="text.secondary">
+            $ {product.price}
+          </Typography>
+          <Chip label={product.categoryName} color="primary" />
+          <Divider sx={{ my: 4 }} />
+          <Grid container spacing={2} direction={"column"} maxWidth={"240px"}>
+            <Grid item xs={6} md={4}>
+              <TextField
+                type="number"
+                label="Quantity"
+                variant="outlined"
+                inputProps={{ min: 0 }}
+                size="small"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6} md={4}>
+              <Button variant="contained" color="primary" fullWidth>
+                Add to Cart
+              </Button>
+            </Grid>
+          </Grid>
+          <Divider sx={{ my: 4 }} />
+          <Typography variant="body1">{product.description}</Typography>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ProductPage;
