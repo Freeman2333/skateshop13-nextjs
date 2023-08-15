644cb05aa1c5b6db28e8c2357339dc96c7da2f54
Serhii Popov <serhii.popov@codeit.pro>
2023-08-15 11:02:45 +0300

    fixes

--- __tests__/components/FilterSidebar.spec.jsx
+++ __tests__/components/FilterSidebar.spec.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, fireEvent } from "@testing-library/react";
 import ProductFilterSidebar from "@/components/product-filter-sidebar";
 import { useRouter } from "next/navigation";
 
@@ -29,36 +28,22 @@ describe("ProductFilterSidebar", () => {
     screen = render(<ProductFilterSidebar categories={categories} />);
   });
 
-  it("sidebar opens when 'Filter' button is clicked", async () => {
+  it("sidebar opens when 'Filter' button is clicked", () => {
     const filterButton = screen.getByRole("button", { name: "Filter" });
-    await userEvent.click(filterButton);
+    fireEvent.click(filterButton);
     const sidebar = screen.queryByRole("heading", { name: /filters/i });
     expect(sidebar).toBeInTheDocument();
   });
 
-  it("sidebar closes when close button is clicked", async () => {
+  it("sidebar closes when close button is clicked", () => {
     const filterButton = screen.getByRole("button", { name: "Filter" });
-    await userEvent.click(filterButton);
+    fireEvent.click(filterButton);
     let sidebar = screen.queryByRole("heading", { name: /filters/i });
     expect(sidebar).toBeInTheDocument();
 
     const closeButton = screen.getByTestId("close-icon");
-    await userEvent.click(closeButton);
+    fireEvent.click(closeButton);
     sidebar = screen.queryByRole("heading", { name: /filters/i });
     expect(sidebar).not.toBeInTheDocument();
-  });
-
-  it.skip("Min Price change updates router push", async () => {
-    const filterButton = screen.getByRole("button", { name: "Filter" });
-    await userEvent.click(filterButton);
-
-    const minPriceInput = screen.getByLabelText("Min Price");
-
-    userEvent.clear(minPriceInput);
-    userEvent.type(minPriceInput, "50"); // Type the new value '50'
-
-    // Check if router.push is called with the expected URL
-    expect(useRouter().push).toHaveBeenCalledWith("/?price_range=50-500");
-    expect(useRouter().push).toHaveBeenCalledTimes(1);
   });
 });
